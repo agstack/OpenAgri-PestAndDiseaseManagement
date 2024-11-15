1fa411128f2d97d25aafe566ef767b8eaabe7ed1
stefan.drobic <stefan.drobic@vizlore.com>
2024-11-15 13:48:21 +0100

    moved list of acceptable column names to separate utils dir, instead of instantiating array each time in api call

--- app/api/api_v1/endpoints/data.py
+++ app/api/api_v1/endpoints/data.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, File, UploadFile, HTTPException
 from sqlalchemy.orm import Session
 
 import crud
+import utils
 from api import deps
 from models import User
 from schemas import Message, CreateData, CreateDataset
@@ -59,14 +60,7 @@ async def upload(
     # Parse the .csv headers
     headers = next(csv_reader)
 
-    possible_column_names = [
-        "date", "time", "parcel_location", "atmospheric_temperature", "atmospheric_temperature_daily_min",
-        "atmospheric_temperature_daily_max", "atmospheric_temperature_daily_average", "atmospheric_relative_humidity",
-        "atmospheric_pressure", "precipitation", "average_wind_speed", "wind_direction", "wind_gust",
-        "leaf_relative_humidity", "leaf_temperature", "leaf_wetness", "soil_temperature_10cm", "soil_temperature_20cm",
-        "soil_temperature_30cm", "soil_temperature_40cm", "soil_temperature_50cm", "soil_temperature_60cm",
-        "solar_irradiance_copernicus"
-    ]
+    possible_column_names = utils.possible_column_names
 
     # If there are more columns than what is expected, decline the dataset
     if len(headers) > len(possible_column_names):
---
+++ app/utils/data.py
@@ -0,0 +1,8 @@
+possible_column_names = [
+        "date", "time", "parcel_location", "atmospheric_temperature", "atmospheric_temperature_daily_min",
+        "atmospheric_temperature_daily_max", "atmospheric_temperature_daily_average", "atmospheric_relative_humidity",
+        "atmospheric_pressure", "precipitation", "average_wind_speed", "wind_direction", "wind_gust",
+        "leaf_relative_humidity", "leaf_temperature", "leaf_wetness", "soil_temperature_10cm", "soil_temperature_20cm",
+        "soil_temperature_30cm", "soil_temperature_40cm", "soil_temperature_50cm", "soil_temperature_60cm",
+        "solar_irradiance_copernicus"
+    ]
